611b785e2c99356f054dd0529b8eaa23cd7391de
Edinaldo Gomes <113068257+edinaldogomes@users.noreply.github.com>
2023-10-19 15:32:35 -0300

    Inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+function openAdm() {
+    window.open('adm.html', '_blank');
+}
+
+function openDs(){
+    window.open('ds.html', '_blank');
+}
+
+function openEnf(){
+    window.open('enf.html', '_blank');
+}
+
+function openRedes(){
+    window.open('redes.html', '_blank');
+}
+
+function openExternalPage() {
+    window.open('https://forms.gle/bLKKUKoNVtW8ojoE8', '_blank');
+}
